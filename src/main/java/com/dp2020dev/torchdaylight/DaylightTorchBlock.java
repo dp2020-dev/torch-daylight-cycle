e32d32d63bb121d16e318a6a6e88c64f9dbbee26
dp2020-dev <ebost2004@yahoo.co.uk>
2026-08-21 17:48:45 +0100

    Sub-step 2b: daylight/weather-aware torch swap working

--- src/main/java/com/dp2020dev/torchdaylight/DaylightTorchBlock.java
+++ src/main/java/com/dp2020dev/torchdaylight/DaylightTorchBlock.java
@@ -1,12 +1,33 @@
 package com.dp2020dev.torchdaylight;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.TorchBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class DaylightTorchBlock extends TorchBlock {
 
     public DaylightTorchBlock(SimpleParticleType flameParticle, BlockBehaviour.Properties properties) {
         super(flameParticle, properties);
+    }
+
+    @Override
+    protected void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        boolean daylit = level.isBrightOutside() && level.canSeeSky(pos);
+        boolean isBright = this == ModBlocks.TORCH_BRIGHT;
+
+        if (isBright && daylit) {
+            swapTo(level, pos, ModBlocks.TORCH_SMOULDERING);
+        } else if (!isBright && !daylit) {
+            swapTo(level, pos, ModBlocks.TORCH_BRIGHT);
+        }
+    }
+
+    private void swapTo(ServerLevel level, BlockPos pos, Block target) {
+        level.setBlockAndUpdate(pos, target.defaultBlockState());
     }
 }
